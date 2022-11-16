82bd29e8682810419aaae198946c3945253ba837
kiboma <omayioben@gmail.com>
2022-11-16 08:23:42 +0300

    Add navbar and footer'

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,11 @@ class GroupsController < ApplicationController
 
   # GET /groups or /groups.json
   def index
-    @groups = Group.all
+    if current_user
+      @groups = Group.all
+     else
+      redirect_to new_user_session_path, notice: 'You are not logged in.'
+    end
   end
 
   # GET /groups/1 or /groups/1.json
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
 class HomeController < ApplicationController
-  def index; end
+  def index;
+    @name = current_user.name
+  end
 end
