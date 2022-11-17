87e59f45cc0de6ee9fcb6170e208503d74236715
kiboma <omayioben@gmail.com>
2022-11-17 14:15:07 +0300

    Create budget and update models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_105603) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_110822) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_105603) do
     t.float "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "group_id"
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_budgets_on_author_id"
+    t.index ["group_id"], name: "index_budgets_on_group_id"
   end
 
   create_table "groups", force: :cascade do |t|
