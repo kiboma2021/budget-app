cafd5bf9d4bb1baf1220fca89bf6e65ef1261d07
kiboma <omayioben@gmail.com>
2022-11-18 14:28:08 +0300

    Resize icons

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   resources :groups
   root 'home#index'
   devise_for :users
+
+  resources :groups, :path => 'categories' do
+    resources :budgets, :path => 'transactions'
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
