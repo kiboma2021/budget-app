3e9521aa7af3232c41db86c0eea897b4fb3aa06c
kiboma <omayioben@gmail.com>
2022-11-19 21:24:02 +0300

    Add group tests

--- spec/group_spec.rb
+++ spec/group_spec.rb
@@ -15,10 +15,21 @@ RSpec.describe 'Group', type: :model do
     @group.save
   end
 
-  describe 'Group validations should work' do
+  describe 'Group validations' do
     it 'should have a name' do
       @group.name = nil
       expect(@group).to_not be_valid
     end
+
+    it 'Group name should exist' do
+      expect(@group.name).to eq(@group.name)
+    end
+
+    it 'icon should be present' do
+      @group.icon = nil
+      expect(@group).to_not be_valid
+    end
+
   end
 end
+
